8e9a270a1293445f54b5240de52dd733269c109a
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2012-04-11 19:14:20 +0800

    Black shiny buttons.

---
+++ Urban Navigation/UNButton.h
@@ -0,0 +1,13 @@
+//
+//  UNButton.h
+//  Urban Navigation
+//
+//  Created by Samuel Williams on 11/04/12.
+//  Copyright (c) 2012 Orion Transfer Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UNButton : UIButton
+
+@end
---
+++ Urban Navigation/UNButton.m
@@ -0,0 +1,89 @@
+//
+//  UNButton.m
+//  Urban Navigation
+//
+//  Created by Samuel Williams on 11/04/12.
+//  Copyright (c) 2012 Orion Transfer Ltd. All rights reserved.
+//
+
+#import "UNButton.h"
+#import <QuartzCore/QuartzCore.h>
+
+@implementation UNButton
+
+- (void)makeShinyButton {
+    // Get the button layer and give it rounded corners with a semi-transparant button
+    CALayer *layer = self.layer;
+    layer.cornerRadius = 8.0f;
+    layer.masksToBounds = YES;
+    layer.borderWidth = 1.0f;
+    layer.borderColor = [UIColor colorWithWhite:0.4f alpha:0.2f].CGColor;
+	
+    // Create a shiny layer that goes on top of the button
+    CAGradientLayer *shineLayer = [CAGradientLayer layer];
+    shineLayer.frame = layer.bounds;
+    // Set the gradient colors
+    shineLayer.colors = [NSArray arrayWithObjects:
+                         (id)[UIColor colorWithWhite:1.0f alpha:0.4f].CGColor,
+                         (id)[UIColor colorWithWhite:1.0f alpha:0.2f].CGColor,
+                         (id)[UIColor colorWithWhite:0.75f alpha:0.2f].CGColor,
+                         (id)[UIColor colorWithWhite:0.4f alpha:0.2f].CGColor,
+                         (id)[UIColor colorWithWhite:1.0f alpha:0.4f].CGColor,
+                         nil];
+    // Set the relative positions of the gradien stops
+    shineLayer.locations = [NSArray arrayWithObjects:
+                            [NSNumber numberWithFloat:0.0f],
+                            [NSNumber numberWithFloat:0.5f],
+                            [NSNumber numberWithFloat:0.5f],
+                            [NSNumber numberWithFloat:0.8f],
+                            [NSNumber numberWithFloat:1.0f],
+                            nil];
+	
+    // Add the layer to the button
+    [layer addSublayer:shineLayer];	
+}
+
++ (UIImage *) imageFromColor:(UIColor *)color {
+    CGRect rect = CGRectMake(0, 0, 1, 1);
+    
+	UIGraphicsBeginImageContext(rect.size);
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    CGContextSetFillColorWithColor(context, [color CGColor]);
+    CGContextFillRect(context, rect);
+    UIImage * image = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+	
+    return image;
+}
+
+- (void)standardBlackShinyButton {
+	[self makeShinyButton];
+	
+	[self setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+	[self setTitleColor:[UIColor grayColor] forState:UIControlStateSelected];
+	
+	UIColor * backgroundColor = [UIColor colorWithRed:0.1 green:0.1 blue:0.1 alpha:1.0];
+	[self setBackgroundImage:[UNButton imageFromColor:backgroundColor] forState:UIControlStateNormal];
+}
+
+- (id)initWithFrame:(CGRect)frame {
+    self = [super initWithFrame:frame];
+	
+    if (self) {
+		[self standardBlackShinyButton];
+    }
+	
+    return self;
+}
+
+- (id)initWithCoder:(NSCoder *)aDecoder {
+	self = [super initWithCoder:aDecoder];
+	
+	if (self) {
+		[self standardBlackShinyButton];
+	}
+	
+	return self;
+}
+
+@end
